b7a53cc168e6aa671ce2226f4594cc144a56c06d
Roger Stebler <Roger.Stebler@bsi-software.com>
2025-08-22 13:21:00 +0200

    Move AbstractUserPreferencesStorageService from shared to client
    As part of the move toward a stateless server session, the
    AbstractUserPreferencesStorageService - which stores the preferences on
    the session - has been moved from shared to client
    
    423389

--- org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/services/common/prefs/Preferences.java
+++ org.eclipse.scout.rt.shared/src/main/java/org/eclipse/scout/rt/shared/services/common/prefs/Preferences.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -67,7 +67,7 @@ public class Preferences implements IPreferences {
   private final transient FastListenerList<IPreferenceChangeListener> m_eventListeners;
   private boolean m_dirty;
 
-  protected Preferences(String name, ISession userScope) {
+  public Preferences(String name, ISession userScope) {
     this(name, userScope, new LinkedHashMap<>(), false);
   }
 
